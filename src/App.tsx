f02031af32135eb114ba2ebe7eab2753d4fa7d68
yuzu <s.yuzu441@gmail.com>
2022-11-09 20:00:46 +0900

    tanstack-router works

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import {
+  Outlet,
+  RouterProvider,
+  createReactRouter,
+  createRouteConfig,
+} from "@tanstack/react-router";
+
+const routeConfig = createRouteConfig().createChildren((createRoute) => [
+  createRoute({
+    path: "/",
+    element: <Index />,
+  }),
+  createRoute({
+    path: "about",
+    element: <About />,
+  }),
+]);
+
+const router = createReactRouter({ routeConfig });
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <>
+      <RouterProvider router={router}>
+        <div>
+          <router.Link to="/">Home</router.Link>
+          <router.Link to="/about">About</router.Link>
+        </div>
+        <hr />
+        <Outlet />
+      </RouterProvider>
+    </>
+  );
 }
 
-export default App
+export default App;
+
+function Index() {
+  return (
+    <div>
+      <h3>Welcome Home!</h3>
+    </div>
+  );
+}
+
+function About() {
+  return <div>Hello from About!</div>;
+}
